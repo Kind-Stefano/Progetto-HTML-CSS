aa5979a6cb1b0d58de34b956ca4294785d70c0e7
StefanoBearnhart <stebernardi91@gmail.com>
2024-04-29 11:55:24 +0200

    Ho spostato lo script di about.html nel suo foglio .js dedicato

--- Assets/Javascript/about.js
+++ Assets/Javascript/about.js
@@ -6,3 +6,23 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
+(function(){
+  emailjs.init("RBpJZE3p6UK_h-Npn");
+})();
+
+document.getElementById('Myform').addEventListener('submit', function(event) {
+ event.preventDefault();
+ var formData = {
+     name: this.nome.value,
+     email: this.email.value,
+     message: this.messaggio.value
+ };
+ // Invia i dati tramite EmailJS
+ emailjs.send('test_42i0ka', 'template_qc2wkkl', formData)
+     .then(function(response) {
+         console.log('Messaggio inviato con successo!', response);
+     }, function(error) {
+         console.log('Errore durante l\'invio del messaggio:', error);
+     });
+});
